0e037a7264a299db955112bb53127f2b7208098e
Michael Kreil <github@michael-kreil.de>
2022-02-02 10:36:31 +0100

    use promises

--- bin/1_fetch_data.js
+++ bin/1_fetch_data.js
@@ -8,21 +8,27 @@ const {resolve} = require('path');
 
 const folder = 'https://storage.googleapis.com/brdata-public-data/rki-corona-archiv/2_parsed/';
 
-download(folder+'index.txt', list => {
+start()
+
+async function start() {
+	console.log('download index');
+	let list = await download(folder+'index.txt');
 	list = list.toString().split('\n');
 	list.sort();
 	let file = list.pop();
 
-	download(folder+file, data => {
-		fs.writeFileSync(resolve(__dirname, '../data/', file), data);
-	})
-});
+	console.log('download data', file);
+	let data = await download(folder+file);
+	fs.writeFileSync(resolve(__dirname, '../data/', file), data);
+}
 
-function download(url, cb) {
-	https.get(url, { headers: { 'User-Agent':'curl/7.64.1', 'Accept':'*/*' }}, res => {
-		let data = [];
-		res.on('data', chunk => data.push(chunk));
-		res.on('end', () => cb(Buffer.concat(data)));
-		res.on('error', err => console.log('Error: ' + err.message));
+function download(url) {
+	return new Promise(resolve => {
+		https.get(url, { headers: { 'User-Agent':'curl/7.64.1', 'Accept':'*/*' }}, response => {
+			let data = [];
+			response.on('data', chunk => data.push(chunk));
+			response.on('end', () => resolve(Buffer.concat(data)));
+			response.on('error', err => console.log('Error: ' + err.message));
+		})
 	})
 }
